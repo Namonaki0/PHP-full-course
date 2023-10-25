d58a1c1386b9f7ba82597302720660d53c63428d
Andre <andreferreira@live.co.uk>
2023-10-25 22:21:33 +0100

    super global variable

---
+++ _SERVER/index.php
@@ -0,0 +1,34 @@
+<?php
+
+//? $_SERVER = SGB that contains headers, paths and script locations.
+//?            The entries in this array are created by the web server.
+//?            Shows nearly everything you need to know about the current web page env.
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server</title>
+</head>
+
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <input type="text" placeholder="username" name="username">
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+
+</html>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    foreach ($_SERVER as $key => $value) {
+        echo "{$key} = {$value} <br>";
+    }
+    // echo "Test";
+}
